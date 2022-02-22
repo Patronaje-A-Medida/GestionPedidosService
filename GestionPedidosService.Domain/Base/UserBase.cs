91a3151cd00af02ac4978eb819c31f5105ce3980
Jeiel <jeiel.disas@gmail.com>
2022-02-22 02:47:22 -0500

    agregado clase base para usuarios

---
+++ GestionPedidosService.Domain/Base/UserBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionPedidosService.Domain.Base
+{
+    public class UserBase
+    {
+        public string Id { get; set; }
+        public string NameUser { get; set; }
+        public string LastNameUser { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public bool EmailVerified { get; set; }
+        public string Status { get; set; }
+    }
+}
