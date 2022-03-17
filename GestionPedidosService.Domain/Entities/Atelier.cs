b6bb5def6ff41a7131707a8d23d0929dcdd59391
Jeiel <jeiel.disas@gmail.com>
2022-03-17 02:26:10 -0500

    #12 actualización de entitades del negocio

---
+++ GestionPedidosService.Domain/Entities/Atelier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionPedidosService.Domain.Entities
+{
+    public class Atelier
+    {
+        public int Id { get; set; }
+        public string NameAtelier { get; set; }
+        public string RucAtelier { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Address { get; set; }
+        public string DescriptionAtelier { get; set; }
+
+        // relations
+        public IEnumerable<UserAtelier> Employees { get; set; }
+    }
+}
--- GestionPedidosService.Domain/Entities/Garment.cs
+++ GestionPedidosService.Domain/Entities/Garment.cs
@@ -9,7 +9,7 @@ namespace GestionPedidosService.Domain.Entities
         public int Id { get; set; }
         public string CodeGarment { get; set; }
         public string NameGarment { get; set; }
-        //public string Description { get; set; }
+        public string Description { get; set; }
         public double FirstRangePrice { get; set; }
         public double SecondRangePrice { get; set; }
         public bool Available { get; set; }
