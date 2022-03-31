88e12d240287f33604f70f9ddbf1e3759aa9977f
Jeiel <jeiel.disas@gmail.com>
2022-03-31 06:33:39 -0500

    #22 renombrado de modelos y creacion de GarmentMinMobile dto

---
+++ GestionPedidosService.Domain/Models/Garments/GarmentReadMobile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionPedidosService.Domain.Models.Garments
+{
+    public class GarmentReadMobile
+    {
+        public string NameGarment { get; set; }
+        public string Description { get; set; }
+        public double FirstRangePrice { get; set; }
+        public double SecondRangePrice { get; set; }
+        public string Category { get; set; }
+        public string NameAtelier { get; set; }
+        public string AtelierAddress { get; set; }
+        public IEnumerable<string> Colors { get; set; }
+        public IEnumerable<string> Fabrics { get; set; }
+        public IEnumerable<string> Occasions { get; set; }
+        public IEnumerable<string> Images { get; set; }
+    }
+}
