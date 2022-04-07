d8dfe54852a994212dc0a0e41ce69f43a711fa0b
Jeiel Tarazona Lovera <jeiel.disas@gmail.com>
2022-04-07 12:25:22 -0500

    #28 creacion de modelos order-create y order-detail-create

---
+++ GestionPedidosService.Domain/Models/Orders/OrderCreate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionPedidosService.Domain.Models.Orders
+{
+    public class OrderCreate
+    {
+        public DateTime OrderDate { get; set; }
+        public int UserClientId { get; set; }
+        public ICollection<OrderDetailCreate> Details { get; set; }
+    }
+}
---
+++ GestionPedidosService.Domain/Models/Orders/OrderDetailCreate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionPedidosService.Domain.Models.Orders
+{
+    public class OrderDetailCreate
+    {
+        public int GarmentId { get; set; }
+        public string Color { get; set; }
+        public int Quantity { get; set; }
+    }
+}
