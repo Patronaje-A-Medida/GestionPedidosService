81fa11d8a796095fd61976e229c903c01e084c0e
Jeiel Tarazona Lovera <jeiel.disas@gmail.com>
2022-04-07 12:27:03 -0500

    #28 agregado funciones de obtener atelier por garmentId y obtener ultimo code order registrado

--- GestionPedidosService.Persistence/Repositories/Interfaces/IAtelierRepository.cs
+++ GestionPedidosService.Persistence/Repositories/Interfaces/IAtelierRepository.cs
@@ -1,9 +1,11 @@
 ﻿using GestionPedidosService.Domain.Entities;
 using GestionPedidosService.Persistence.Interfaces;
+using System.Threading.Tasks;
 
 namespace GestionPedidosService.Persistence.Repositories.Interfaces
 {
     public interface IAtelierRepository : IRepository<Atelier>
     {
+        Task<Atelier> GetByGarmentId(int garmentId);
     }
 }
