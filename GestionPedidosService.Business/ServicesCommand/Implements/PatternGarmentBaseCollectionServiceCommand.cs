a4777a79f4c147fa957934773018d5dad0d29df5
Jeiel <jeiel.disas@gmail.com>
2022-04-14 11:35:14 -0500

    #31 creacion de servicios para patter-garment-base

---
+++ GestionPedidosService.Business/ServicesCommand/Implements/PatternGarmentBaseCollectionServiceCommand.cs
@@ -0,0 +1,30 @@
+﻿using GestionPedidosService.Business.ServicesCommand.Interfaces;
+using GestionPedidosService.Domain.Collections;
+using GestionPedidosService.Persistence.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPedidosService.Business.ServicesCommand.Implements
+{
+    public class PatternGarmentBaseCollectionServiceCommand : IPatternGarmentBaseCollectionServiceCommand
+    {
+        private readonly IPatternGarmentBaseCollectionRepository _repository;
+
+        public PatternGarmentBaseCollectionServiceCommand(IPatternGarmentBaseCollectionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Add(PatternGarmentBase patternGarmentBase)
+        {
+            await _repository.Add(patternGarmentBase);
+        }
+
+        public async Task Add(IEnumerable<PatternGarmentBase> patternGarmentBases)
+        {
+            await _repository.Add(patternGarmentBases);
+        }
+    }
+}
---
+++ GestionPedidosService.Business/ServicesCommand/Interfaces/IPatternGarmentBaseCollectionServiceCommand.cs
@@ -0,0 +1,14 @@
+﻿using GestionPedidosService.Domain.Collections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPedidosService.Business.ServicesCommand.Interfaces
+{
+    public interface IPatternGarmentBaseCollectionServiceCommand
+    {
+        Task Add(PatternGarmentBase patternGarmentBase);
+        Task Add(IEnumerable<PatternGarmentBase> patternGarmentBases);
+    }
+}
